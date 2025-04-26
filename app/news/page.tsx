3d8c11dcc9300596e27aa1517c6c599e279d1f5a
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-04-27 00:53:50 +0100

    added news pages

---
+++ app/news/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function News() {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/first-news">First News Item</Link>
+        </li>
+        <li>
+          <Link href="/news/second-news">Second News Item</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
