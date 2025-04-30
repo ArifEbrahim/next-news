8d411cfc0b60b9068ad43fe1858bec9aa2eed802
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-04-30 02:52:36 +0100

    added catch all segment

---
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -0,0 +1,29 @@
+import NewsList from '@/components/news-list'
+import { getArticlesForYear, getArticleYears } from '@/lib/articles'
+import Link from 'next/link'
+
+export default async function FilteredNewsPage({
+  params
+}: {
+  params: Promise<{ filter: string[] }>
+}) {
+  const { filter } = await params
+  const articles = await getArticlesForYear(filter[0])
+  const links = await getArticleYears()
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map(link => (
+              <li key={link}>
+                <Link href={`/archive/${link}`}>{link}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <NewsList articles={articles} />
+    </>
+  )
+}
