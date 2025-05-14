ab216718cee1e183438d2ca522d5b35265a243c2
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-05-14 03:36:45 +0100

    added data fetching from server

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,13 +1,43 @@
-import NewsList from '@/components/news-list'
-import { getAllArticles } from '@/lib/articles'
+'use client'
 
-export default async function NewsPage() {
-  const articles = await getAllArticles()
+import NewsList from '@/components/news-list'
+import { useState, useEffect } from 'react'
+
+export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>()
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true)
+      const response = await fetch('http://localhost:8080/news')
+
+      if (!response.ok) {
+        setError('Failed to fetch news')
+        setIsLoading(false)
+      }
+
+      const data = await response.json()
+      setIsLoading(false)
+      setArticles(data)
+    }
+
+    fetchNews()
+  }, [])
+
+  if (isLoading) {
+    return <>Loading...</>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
 
   return (
     <>
       <h1>News Page</h1>
-      <NewsList articles={articles} />
+      {articles.length > 0 && <NewsList articles={articles} />}
     </>
   )
 }
